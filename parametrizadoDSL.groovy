a8b7c4e324afc2a20a4ce4a007f9515e80cc170d
Jessica Estrada <92817753+puppycat1401@users.noreply.github.com>
2021-11-04 18:50:14 -0600

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,40 @@
+job('ejemplo-job-github'){
+	description('JOB DE EJEMPLO')
+    scm {
+        git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { 
+            node / gitConfigName('Jessica Estrada')
+            node / gitConfigEmail('jessica.estrada.robreno@gmail.com')
+        }
+	}
+  	parameters{
+  		stringParam('nombre', defaultValue = "Jess", description = "Paramtro de cadena")
+      	choiceParam('planeta', ['tierra','jupiter', 'urano','pluton'])
+      	booleanParam('agente', false)
+  	}
+    triggers{
+      cron('H/7 * * * *')
+    }
+    steps{
+      shell("bash jobscript.sh")
+    }
+  	publishers{
+  		mailer('Jessica.Lissette.Estrada.Robreno@kyndryl.com',true,true)
+       	
+      	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+          	notifyNotBuilt(false)
+          	notifyUnstable(false)
+          	notifyBackToNormal(true)
+            notifySuccess(false)
+          	notifyRepeatedFailure(false)
+          	startNotification(false)
+          	includeTestSummary(false)
+          	includeCustomMessage(false)
+          	sendAs(null)
+           	commitInfoChoice('NONE')
+          	teamDomain(null)
+          	authToken(null)
+        }
+	}
+}
